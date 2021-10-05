e17482e42061a6fb5c1f216af99708c31ff72dc1
Murillo Lima <murillo_smit@hotmail.com>
2021-10-05 12:58:56 -0300

    Page login

---
+++ resources/views/admin/layouts/head.blade.php
@@ -0,0 +1,27 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Mercado</title>
+<link rel="icon" href="{{ asset('assets/admin/icon.png') }}" type="image/png" sizes="57x57">
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="{{ asset('assets/admin/plugins/fontawesome-free/css/all.min.css') }}">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{ asset('assets/admin/plugins/ekko-lightbox/ekko-lightbox.css') }}">
+
+<link rel="stylesheet" href="{{ asset('assets/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('assets/admin/dist/css/adminlte.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/global.css') }}">
+
+<!-- Google Font: Source Sans Pro -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+<style>
+    div#loader {
+        position: fixed;
+        left: 0px;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+        z-index: 9999;
+        background: url('{{url('assets/admin/dist/img/4.gif')}}') 50% 50% no-repeat white;
+    }
+</style>
